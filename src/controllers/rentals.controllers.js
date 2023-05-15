83065216c0421f337400f7c239adc3f1f0677976
Marcos Couto <marcoscouto1997@gmail.com>
2023-05-15 15:53:00 -0300

    função que insere clientes implementada

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import { db } from "../database/database.connection.js";
 
 export async function getRentals(req, res) {
@@ -18,8 +19,21 @@ export async function insertRentals(req, res) {
     const { customerId, gameId, daysRented } = req.body
 
     try {
-        await db.query(`INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFree")
-        VALUES(     );`, [customerId, gameId, daysRented])
+    
+        const findGameId = await db.query(`SELECT * FROM games WHERE id = $1;`,[gameId])
+        const findCustomerId = await db.query(`SELECT * FROM customers WHERE id=$1;`, [customerId])
+
+        if (findCustomerId.rows.length === 0 || findGameId.rows.length === 0) return res.sendStatus(400)
+
+        const rentDate = dayjs().format("YYYY-MM-DD")
+        const originalPrice = daysRented * findGameId.rows[0].pricePerDay
+
+        const checkGame = await db.query(`SELECT * FROM rentals WHERE "gameId"=$1 AND "returnDate" is null;`, [gameId])
+
+        if(checkGame.rows.length > findGameId.rows[0].stockTotal) return res.sendStatus(400)
+
+        await db.query(`INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee")
+        VALUES($1, $2, $3, $4, null, $5, null);`, [customerId, gameId, rentDate, daysRented, originalPrice])
         res.sendStatus(201)
     } catch (err) {
         res.sendStatus(500)
--- src/routes/rentals.routes.js
+++ src/routes/rentals.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import { finalizeRentals, getRentals, insertRentals } from "../controllers/rentals.controllers.js"
-
+import { validateSchema } from "../middlewares/validateSchema.middlewares.js"
+import { rentalsSchema } from "../schemas/rentals.schemas.js"
 const rentalsRouter = Router()
 
 rentalsRouter.get("/rentals", getRentals)
 
-rentalsRouter.post("/rentals", insertRentals)
+rentalsRouter.post("/rentals", validateSchema(rentalsSchema), insertRentals)
 
 rentalsRouter.post("/rentals/:id/return", finalizeRentals)
 
