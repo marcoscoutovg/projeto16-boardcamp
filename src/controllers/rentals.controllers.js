9cea460253cc2f07e5e1e527f60e40eacd16a164
Marcos Couto <marcoscouto1997@gmail.com>
2023-05-15 16:30:06 -0300

    listar aluguéis implementado

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -7,8 +7,36 @@ export async function getRentals(req, res) {
 
 
     try {
-        const listRentals = await db.query(`SELECT * FROM rentals;`)
-        res.send(listRentals.rows)
+        const rentals = await db.query(`SELECT rentals.*, customers.id AS customer_id,
+        customers.name AS customer_name,
+        games.id AS game_id, games.name AS game_name FROM rentals 
+        JOIN customers ON rentals."customerId" = customers.id 
+        JOIN games ON rentals."gameId" = games.id;`)
+        
+        const listRentals = rentals.rows.map((l) => ({
+            id: l.id,
+            customerId: l.customerId,
+            gameId: l.gameId,
+            rentDate: l.rentDate,
+            daysRented: l.daysRented,
+            returnDate: l.returnDate,
+            originalPrice: l.originalPrice,
+            delayFee: l.delayFee,
+            customer: {
+                id: l.customer_id,
+                name: l.customer_name
+            },
+            game: {
+                id: l.game_id,
+                name: l.game_name
+            }
+        }))
+
+        console.table(listRentals)
+
+        
+
+        res.status(200).send(listRentals)
     } catch (err) {
         res.sendStatus(500)
     }
@@ -19,8 +47,8 @@ export async function insertRentals(req, res) {
     const { customerId, gameId, daysRented } = req.body
 
     try {
-    
-        const findGameId = await db.query(`SELECT * FROM games WHERE id = $1;`,[gameId])
+
+        const findGameId = await db.query(`SELECT * FROM games WHERE id = $1;`, [gameId])
         const findCustomerId = await db.query(`SELECT * FROM customers WHERE id=$1;`, [customerId])
 
         if (findCustomerId.rows.length === 0 || findGameId.rows.length === 0) return res.sendStatus(400)
@@ -28,9 +56,9 @@ export async function insertRentals(req, res) {
         const rentDate = dayjs().format("YYYY-MM-DD")
         const originalPrice = daysRented * findGameId.rows[0].pricePerDay
 
-        const checkGame = await db.query(`SELECT * FROM rentals WHERE "gameId"=$1 AND "returnDate" is null;`, [gameId])
+        const checkGame = await db.query(`SELECT * FROM rentals WHERE "gameId"=$1 AND "returnDate" = null;`, [gameId])
 
-        if(checkGame.rows.length > findGameId.rows[0].stockTotal) return res.sendStatus(400)
+        if (checkGame.rows.length > findGameId.rows[0].stockTotal) return res.sendStatus(400)
 
         await db.query(`INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee")
         VALUES($1, $2, $3, $4, null, $5, null);`, [customerId, gameId, rentDate, daysRented, originalPrice])
